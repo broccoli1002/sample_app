38a9d98b9b83b630e0a56b202878aba62a62ea4a
Kenta Nakayama <ubuntu@ip-10-0-1-54.ap-northeast-1.compute.internal>
2022-05-14 16:01:33 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    # => app/views/{リソース名}/@{アクション名}.html.erb
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
